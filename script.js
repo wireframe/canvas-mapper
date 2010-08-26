f984be3230ddede29b4a0f88aef35405b3d02cee
Ryan Sonnek <ryan@socialcast.com>
2010-08-25 22:27:33 -0500

    use translation to draw token

--- script.js
+++ script.js
@@ -22,17 +22,21 @@ Viewport = function(canvas, grid) {
   this.width = canvas.width();
   this.height = canvas.height();
 
-  this.pointerX = 0;
-  this.pointerY = 0;
+  this.mouseX = 0;
+  this.mouseY = 0;
   this.grid = grid;
   this.context = canvas[0].getContext("2d");
 
   var self = this;
   canvas.mousemove(function(e) {
-    self.pointerX = e.pageX - canvas.position().left;
-    self.pointerY = e.pageY - canvas.position().top;
+    self.mouseX = e.pageX - canvas.position().left;
+    self.mouseY = e.pageY - canvas.position().top;
   });
-
+  canvas.mousedown(function(e) {
+    if (self.token.isWithinBounds(self.mouseX, self.mouseY)) {
+      alert('here');
+    }
+  });
   var scale = 1;
   var originx = 0;
   var originy = 0;
@@ -61,10 +65,10 @@ Viewport.prototype = {
     this.grid.draw(this.context, this);
 
     this.context.fillStyle = "rgba(255, 165, 0, 0.4)";
-    var hoverSquare = this.grid.squareAt(this.pointerX, this.pointerY, this);
+    var hoverSquare = this.grid.squareAt(this.mouseX, this.mouseY, this);
     this.context.fillRect(hoverSquare.x, hoverSquare.y, this.grid.squareSize, this.grid.squareSize);
 
-    this.token.draw(this.context);
+    this.token.draw(this.context, this);
     this.token.positionX += 1;
     this.token.positionY += 1;
   }
@@ -128,20 +132,21 @@ Token = function(image, grid) {
   this.grid = grid;
 };
 Token.prototype = {
-  draw: function(context) {
+  draw: function(context, viewport) {
     context.save();
+    context.translate(this.positionX, this.positionY);
+
     //setup clipping to make image rounded
     context.beginPath();
-    context.arc(this.tokenCenterpoint().x, this.tokenCenterpoint().y, this.tokenRadius(), 0, Math.PI * 2, true);
+    context.arc(this.tokenRadius(), this.tokenRadius(), this.tokenRadius(), 0, Math.PI * 2, true);
     context.clip();
 
-    context.drawImage(this.image, this.positionX, this.positionY, this.grid.squareSize, this.grid.squareSize);
+    context.drawImage(this.image, 0, 0, this.grid.squareSize, this.grid.squareSize);
 
     //draw border around token
-    context.beginPath();
-    context.strokeStyle = this.borderColor();
+    var isMouseHover = context.isPointInPath(viewport.mouseX, viewport.mouseY);
+    context.strokeStyle = isMouseHover ? 'rgba(255, 0, 0, 0.7)' : 'rgba(0, 0, 0, 0.7)';
     context.lineWidth = this.borderWidth;
-    context.arc(this.tokenCenterpoint().x, this.tokenCenterpoint().y, this.tokenRadius(), 0, Math.PI * 2, true);
     context.stroke();
 
     context.restore();
@@ -150,13 +155,13 @@ Token.prototype = {
   borderColor: function() {
     return 'rgba(' + (this.isBloodied ? 255 : 0) + ', 0, 0, 0.7)';
   },
-  tokenCenterpoint: function() {
-    return {
-      x: this.positionX + this.tokenRadius(),
-      y: this.positionY + this.tokenRadius()
-    };
-  },
   tokenRadius: function() {
     return this.grid.squareSize / 2;
+  },
+  isWithinBounds: function(x, y) {
+    return false;
+    // this.context.beginPath();
+    // this.context.arc(this.tokenCenterpoint().x, this.tokenCenterpoint().y, this.tokenRadius(), 0, Math.PI * 2, true);
+    // return this.context.isPointInPath(x, y);
   }
 };
