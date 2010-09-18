a13f5abe06ed2871e4acbd1c47de7d5adc5f0315
Ryan Sonnek <ryan@socialcast.com>
2010-09-18 14:01:50 -0500

    add new token form

--- script.js
+++ script.js
@@ -1,112 +1,19 @@
-Viewport = function(canvas, grid) {
-  this.width = canvas.width();
-  this.height = canvas.height();
-
-  this.mouseX = 0;
-  this.mouseY = 0;
-  this.grid = grid;
-  this.context = canvas[0].getContext("2d");
-
-  var self = this;
-  canvas.mousemove(function(e) {
-    self.mouseX = e.pageX - canvas.position().left;
-    self.mouseY = e.pageY - canvas.position().top;
-  });
-  canvas.mousedown(function(e) {
-    if (self.token.isWithinBounds(self.mouseX, self.mouseY)) {
-      alert('here');
-    }
-  });
-  var scale = 1;
-  var originx = 0;
-  var originy = 0;
-  canvas.mousewheel(function(event, delta) {
-    var mousex = event.clientX - canvas.offsetLeft;
-    var mousey = event.clientY - canvas.offsetTop;
-    var zoom = 1 + delta/2;
-
-    self.context.translate(originx, originy);
-    self.context.scale(zoom, zoom);
-    self.context.translate(
-        -( mousex / scale + originx - mousex / ( scale * zoom ) ),
-        -( mousey / scale + originy - mousey / ( scale * zoom ) )
-    );
-
-    originx = ( mousex / scale + originx - mousex / ( scale * zoom ) );
-    originy = ( mousey / scale + originy - mousey / ( scale * zoom ) );
-    scale *= zoom;
-  });
-
-  this.token = new Token('token.jpg', grid);
-};
-Viewport.prototype = {
-  draw: function() {
-    this.context.clearRect(0, 0, this.width, this.height);
-    this.grid.draw(this.context, this);
-
-    this.context.fillStyle = "rgba(255, 165, 0, 0.4)";
-    var hoverSquare = this.grid.squareAt(this.mouseX, this.mouseY, this);
-    this.context.fillRect(hoverSquare.x, hoverSquare.y, this.grid.squareSize, this.grid.squareSize);
-
-    this.token.draw(this.context, this);
-    this.token.positionX += 1;
-    this.token.positionY += 1;
-  }
-};
-
-Grid = function(image) {
-  this.background = new Image();
-  this.background.src = image;
-
-  this.squareSize = 32;
-  this.lineWidth = 1;
-  this.showGridLines = true;
-};
-Grid.prototype = {
-  draw: function(context, viewport) {
-    context.drawImage(this.background, 0, 0, viewport.width, viewport.height, 0, 0, viewport.width, viewport.height);
-
-    if (this.showGridLines) {
-      context.lineWidth = this.lineWidth;
-      context.strokeStyle = "rgba(100, 100, 100, 1)";
-      for (var column = 0; column < this.visibleColumns(viewport); column++) {
-        for (var row = 0; row < this.visibleRows(viewport); row++) {
-          var square = this.coordinatesForSquare(column, row, viewport);
-          context.strokeRect(square.x, square.y, this.squareSize, this.squareSize);
-        }
-      }
-    }
-  },
-  maxColumns: function() {
-    return Math.floor(this.background.width / this.squareSize);
-  },
-  maxRows: function() {
-    return Math.floor(this.background.height / this.squareSize);
-  },
-  visibleColumns: function(viewport) {
-    return Math.min((viewport.width / this.squareSize), this.maxColumns());
-  },
-  visibleRows: function(viewport) {
-    return Math.min((viewport.height / this.squareSize), this.maxRows());
-  },
-  coordinatesForSquare: function(column, row, viewport) {
-    return {
-      x: column * this.squareSize,
-      y: row * this.squareSize
-    };
-  },
-  squareAt: function(x, y, viewport) {
-    var col = Math.floor(x / this.squareSize);
-    var row = Math.floor(y / this.squareSize);
-    return this.coordinatesForSquare(col, row, viewport);
-  }
-};
 
 Token = Klass(CanvasNode, {
   defaults: {},
   borderWidth: 4,
   borderColor: '#990000',
   borderHoverColor: '#ff0000',
+  bringToFront: function() {
+    var maxZIndex = 0;
+    $.each(this.parent.childNodes, function() {
+      console.log(this.zIndex);
+      if (this.zIndex > maxZIndex) {
+        maxZIndex = this.zIndex;
+      }
+    });
+    this.zIndex = maxZIndex + 1;
+  },
 
   initialize: function(options) {
     CanvasNode.initialize.call(this);
@@ -125,16 +32,13 @@ Token = Klass(CanvasNode, {
     border.makeDraggable();
     var self = this;
     border.when('focus', function(e){
-      this.zIndex += 1;
-    });
-    border.when('blur', function(){
-      this.zIndex -= 1;
+      self.bringToFront();
     });
     border.when('mouseover', function() {
-      this.stroke = self.borderHoverColor;
+      border.stroke = self.borderHoverColor;
     });
     border.when('mouseout', function() {
-      this.stroke = self.borderColor;
+      border.stroke = self.borderColor;
     });
     this.append(border);
 
@@ -257,4 +161,11 @@ $(function() {
   $('#tokenList').append(token2.initiativeMarkup());
 
   $('#tokenList').sortable();
+
+  $('#newToken').submit(function() {
+    var newToken = new Token({image: $(this).find('option:selected').val(), name: $(this).find('input').val()});
+    grid.append(newToken);
+    $('#tokenList').append(newToken.initiativeMarkup());
+    return false;
+  });
 });
